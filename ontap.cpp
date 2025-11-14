1f3c4f64e224911c1259e9840eaab499001f2378
mozzanhat <nguyenminhnhat211203@gmail.com>
2025-11-14 21:28:46 +0700

    init2

--- ontap.cpp
+++ ontap.cpp
@@ -17,9 +17,16 @@ int main() {
 	std::cin >> a;
 	std::cout << "Nhap b: ";
 	std::cin >> b;
+	std::cout << "Nhap c: ";
+	std::cin >> c;
 	std::cout << "Tong: " << tong(a, b) << std::endl;
 	std::cout << "Hieu: " << tru(a, b) << std::endl;
 	std::cout << "Tich: " << nhan(a, b) << std::endl;
 	std::cout << "Thuong: " << chia(a, b) << std::endl;
+
+	std::cout << "Tong: " << tong(a, c) << std::endl;
+	std::cout << "Hieu: " << tru(a, c) << std::endl;
+	std::cout << "Tich: " << nhan(c, b) << std::endl;
+	std::cout << "Thuong: " << chia(c, b) << std::endl;
 	return 0;
 }
